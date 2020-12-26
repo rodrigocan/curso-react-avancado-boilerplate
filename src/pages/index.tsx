afede65b8c5141fb74ab6972a018869181c1b46e
Rodrigo Candido <rodrigo@DESKTOP-U09JB93.localdomain>
2020-12-25 21:42:36 -0300

    Final da aula 11

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+export default function Home({ title = 'React Avançado!' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          React Avançado!
+          {title}
         </h1>
 
         <p className={styles.description}>
