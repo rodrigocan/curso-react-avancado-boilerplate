8f5280f3fcb90f838578113f111ecb6eac35c157
Rodrigo Candido <rodrigo@DESKTOP-U09JB93.localdomain>
2020-12-28 20:30:30 -0300

    Final da aula 25

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
-  "stories": ['../src/components/**/stories.tsx'],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-controls",
-    "@storybook/addon-knobs/register"
-  ]
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials']
 }
